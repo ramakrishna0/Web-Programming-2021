9694a95249548e8d711ef7d3051d94ef5d7c508b
ramakrishna0 <ramakrishnab.1508@gmail.com>
2021-02-03 13:22:52 -0600

    ICP-3

---
+++ ICP-3/Game/js/script.js
@@ -0,0 +1,38 @@
+$(function() {
+    $("button").click(function () {
+        let userChoice = $(this).attr('id');
+        play(userChoice);
+    });
+})
+
+function play(userChoice) {
+    let oppChoice = Math.random();
+    let opponentChoice='';
+    if (oppChoice<0.34)
+        opponentChoice='rock';
+    else if (oppChoice<0.67)
+        opponentChoice='paper';
+    else
+        opponentChoice='scissors';
+    // alert(userChoice + oppChoice + opponentChoice);
+    if (userChoice === opponentChoice)
+        alert( "Its a tie! Try again");
+    else if (userChoice === "rock")
+        if (opponentChoice === "scissors")
+            alert( "Congrats You Won !!! Your opponent had scissors");
+        else
+            alert("Sorry You Lost !!! Your opponent had Papers");
+    else if (userChoice === "paper")
+        if (opponentChoice === "rock")
+            alert( "Congrats You Won !!! Your opponent had rock");
+        else
+            alert("Sorry You Lost !!! Your opponent had scissors") ;
+    else if (userChoice === "scissors")
+        if (opponentChoice === "rock")
+            alert("Congrats You Won !!! Your opponent had rock");
+        else
+            alert("Sorry You Lost !!! Your opponent had scissors");
+}
+
+
+
