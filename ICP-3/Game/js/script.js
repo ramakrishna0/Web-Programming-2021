a3a7ab2917a20727ecf5e2e3ffc4e016259f42fd
ramakrishna0 <ramakrishnab.1508@gmail.com>
2021-02-03 23:35:09 -0600

    ICP-3

--- ICP-3/Game/js/script.js
+++ ICP-3/Game/js/script.js
@@ -17,21 +17,21 @@ function play(userChoice) {
 
     if (userChoice === opponentChoice)
         alert( "Its a tie! Try again");
-    else if (userChoice === "rock")
-        if (opponentChoice === "scissors")
+    else if (userChoice == "rock")
+        if (opponentChoice == "scissors")
             alert( "Congrats You Won !!! Your opponent had scissors");
         else
             alert("Sorry You Lost !!! Your opponent had Papers");
-    else if (userChoice === "paper")
-        if (opponentChoice === "rock")
+    else if (userChoice == "paper")
+        if (opponentChoice == "rock")
             alert( "Congrats You Won !!! Your opponent had rock");
         else
             alert("Sorry You Lost !!! Your opponent had scissors") ;
-    else if (userChoice === "scissors")
-        if (opponentChoice === "rock")
-            alert("Congrats You Won !!! Your opponent had rock");
+    else if (userChoice == "scissors")
+        if (opponentChoice == "paper")
+            alert("Congrats You Won !!! Your opponent had paper");
         else
-            alert("Sorry You Lost !!! Your opponent had scissors");
+            alert("Sorry You Lost !!! Your opponent had rock");
 }
 
 
