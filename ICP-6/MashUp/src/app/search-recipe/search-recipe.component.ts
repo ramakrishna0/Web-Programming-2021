e947cd11d3e91e29806c3bff1122d3357bf85d08
ramakrishna0 <ramakrishnab.1508@gmail.com>
2021-02-24 19:43:51 -0600

    ICP-6

---
+++ ICP-6/MashUp/src/app/search-recipe/search-recipe.component.ts
@@ -0,0 +1,115 @@
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+interface VenueSearchResponse {
+  meta: {};
+  response: {
+    venues: Venue[];
+    confident: boolean;
+  };
+}
+interface RecipeSearchResponse {
+  count: number;
+  from: number;
+  hits: Hit[];
+  more: boolean;
+  q: string;
+  to: number;
+}
+interface Hit {
+  recipe: Recipe;
+}
+interface Venue {
+  id: string;
+  name: string;
+  location: {
+    address: string;
+    postalCode: string;
+    city: string;
+    state: string;
+    country: string;
+    formattedAddress: string[];
+  };
+  categories: {
+    name: string;
+  };
+  referralId: string;
+  hasPerk: boolean;
+}
+interface Recipe {
+  calories: number;
+  dietLabels: string[];
+  healthLabels: string[];
+  image: string | null;
+  ingredientLines: string[];
+  ingredients: {
+    image: string | null;
+    text: string;
+    weight: number;
+  }[];
+  label: string;
+  source: string;
+  totalTime: number;
+  uri: string;
+  url: string;
+  yield: number;
+}
+
+@Component({
+  selector: 'app-search-recipe',
+  templateUrl: './search-recipe.component.html',
+  styleUrls: ['./search-recipe.component.css']
+})
+export class SearchRecipeComponent implements OnInit {
+  @ViewChild('recipe') recipes: ElementRef;
+  @ViewChild('place') places: ElementRef;
+  recipeValue: any;
+  placeValue: any;
+  venueList = [];
+  recipeList: Recipe[];
+
+  currentLat: any;
+  currentLong: any;
+  geolocationPosition: any;
+
+  constructor(private _http: HttpClient) {
+  }
+
+  ngOnInit() {
+
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.geolocationPosition = position;
+        this.currentLat = position.coords.latitude;
+        this.currentLong = position.coords.longitude;
+      });
+  }
+
+  getVenues() {
+    this.recipeValue = this.recipes.nativeElement.value;
+    this.placeValue = this.places.nativeElement.value;
+    if (this.recipeValue !== null) {
+      /**
+       * Write code to get recipe
+       */
+      this._http.get('https://api.edamam.com/search?q=' + this.recipeValue + '&app_id=db9927b6'
+        + '&app_key=d08fbd177d03c50acff5e26581f28a75')
+          .subscribe(({hits}: RecipeSearchResponse) => {
+            this.recipeList = hits.map(hit => hit.recipe);
+          });
+    }
+    if (this.placeValue != null && this.placeValue !== '' && this.recipeValue != null) {
+      if (this.recipeValue !== '') {
+        /**
+         * Write code to get place
+         */
+        // tslint:disable-next-line:max-line-length
+        this._http.get('https://api.foursquare.com/v2/venues/search/?' + '&client_id=OO2P1I0OMSUPC1RDII0B35LZ1A0XVKNJUPBEPGKRMW1CC3J2'
+            + '&client_secret=KMBRXGGGSIAL5AL25YZIURABMKFSHU4ZVAULIRXB0QB131CM' + '&v=20180928&limit=10&&near=' + this.placeValue + '&query=' + this.recipeValue).
+        subscribe(({response}: VenueSearchResponse) => {
+          this.venueList = response.venues;
+        });
+      }
+    }
+  }
+
+}
